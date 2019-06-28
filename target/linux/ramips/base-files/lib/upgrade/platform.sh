4ab4c3b3bf6ab58906c63771e64e6bbaf4031d13
angelsl <angelsl@in04.sg>
2019-06-29 00:45:16 +0800

    Need fw_{print,set}env in upgrade ramdisk

--- target/linux/ramips/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -5,6 +5,9 @@
 PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
+RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
 platform_check_image() {
 	return 0
 }
